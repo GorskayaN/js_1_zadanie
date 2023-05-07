b118ac78e30e2eefc457686e6f685412a252aae3
GorskayaN <97854636+GorskayaN@users.noreply.github.com>
2023-05-08 00:37:51 +0300

    add 8 zadachek

---
+++ first.js
@@ -0,0 +1,59 @@
+//1
+let cleanerSalary = 40000
+let teacherSalary = 22000
+
+console.log(cleanerSalary + teacherSalary)
+
+//2
+let city = 'Владивосток'
+let num = 2000
+
+console.log(`${city} ${num}`)
+console.log(city + ' ' + num)
+
+//3
+let timeMessager = 128
+let timeLikes = 254
+let timeVk = 137
+let timeYoutube = 201
+
+console.log((timeMessager + timeVk + timeLikes + timeYoutube) / 60)
+
+//4
+let num1 = 2
+let powerNum = 5
+
+console.log(num1 ** powerNum)
+
+//5
+let str =
+  'Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!'
+
+console.log(str)
+
+//6
+const greetings = 'Привет'
+const userName = 'Кот'
+
+console.log(`${greetings}, ${userName}`)
+console.log(greetings + ',' + ' ' + userName)
+
+//7
+let time
+time = 34
+console.log('Старт поездки. Осталось минут: ' + time)
+
+time = time - 15
+console.log('Немного сториз в соцсетях. Осталось минут: ' + time)
+
+time = time - 10
+console.log('Немного не новостей, но событий. Осталось минут: ' + time)
+
+time = 0
+console.log('Вы приехали. Добро пожаловать в Москву')
+
+//8
+let fahrenheit = 451
+let celsius = (fahrenheit - 32) / 1.8
+
+console.log(` ${fahrenheit} градус по Фаренгейту - это ${celsius} градуса по Цельсию.`)
